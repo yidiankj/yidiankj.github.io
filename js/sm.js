0ced7d34380baef45ab55b47eef3ca7c37319b10
yidiankj <144199594+yidiankj@users.noreply.github.com>
2023-10-22 18:40:46 +0800

    Update sm.js

--- js/sm.js
+++ js/sm.js
@@ -1,1 +1,2 @@
 console.log("\n%c 个人导航网站 v1.0 %c https://yidiankj.github.io \n 图片转换 https://convertio.co/zh/webp-png \n 生成二维码 https://cli.im \n 解析二维码 https://2vm.net.cn/home/index/jiema.html \n 修改图片尺寸 https://img.logosc.cn/?fromUrl=www.aiapply.cn \n\n","color: #fadfa3; background: #030307; padding:5px 0;","background: #fadfa3; padding:5px 0;");
+document.body.style.overflow = 'hidden';
